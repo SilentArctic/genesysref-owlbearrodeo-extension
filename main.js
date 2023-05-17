ea2b2c19eaa07bb4e1c3e38dc5780720233a571c
SilentArctic <silentarctic@gmail.com>
2023-05-17 14:03:52 -0400

    point to genesysref.netlify.app

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import './style.css'
 /* GenesysRef Rodeo */
 const app = document.querySelector('#app');
 app.innerHTML = `
-   <iframe src="http://localhost:8080/rodeo" style="height: calc(100vh - 5px); width: 100%; border: none" />
+   <iframe src="http://genesysref.netlify.app/rodeo" style="height: calc(100vh - 5px); width: 100%; border: none" />
 `;
 
 OBR.onReady(() => {
