8f42dc7d3d1175a1de48249cf061127336808ddc
SilentArctic <silentarctic@gmail.com>
2023-05-21 23:42:44 -0400

    create patreonpreview branch

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import './style.css'
 /* GenesysRef Rodeo */
 const app = document.querySelector('#app');
 
-const source = 'https://genesysref.netlify.app/rodeo';
+const source = 'https://patreonpreview--genesysref.netlify.app/rodeo';
 
 app.innerHTML = `
    <iframe src="${source}" style="height: calc(100vh - 5px); width: 100%; border: none" />
